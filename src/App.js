c90b82086882cb8d9e0b8c6b1499f137ca9bca35
corazon4815 <mango4815@knou.ac.kr>
2021-02-26 18:07:51 +0900

    main

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App(){
+
+  let posts = '강남 고기 맛집';
+  function 함수(){
+    return 100
+  }
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
+      <h4> { 함수() } </h4>
     </div>
   )
 }
