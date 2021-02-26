b6eca2c679b4d6826e4b953ecb6ed7b4a43bec1e
corazon4815 <mango4815@knou.ac.kr>
2021-02-26 17:41:35 +0900

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          안녕하세요 
+          안녕하세요 aa
         </p>
         <a
           className="App-link"
