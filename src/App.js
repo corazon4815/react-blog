d5e799b20ac32c0edc991b36a264df6b3a5aaa67
corazon4815 <mango4815@knou.ac.kr>
2021-03-02 18:05:47 +0900

    메인

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
+/*eslint-disable */
+import react, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App(){
+  
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','우동 맛집','여자 코트 추천']);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '개발공부 글';
 
-  let posts = '강남 고기 맛집';
   function 함수(){
-    return 100
+    console.log(1);
   }
+  () => {
+    const [startDate, setStartDate] = useState(new Date());
+    return (
+      <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+    );
+  };
+
+  
+
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 sblog</div>
+   <div className="App">
+      <div className="black-nav"> 
+        <div style={{ color:'white',fontSize:'30px'}}>개발 Blog</div>       
       </div>
-      <h4> { 함수() } </h4>
-    </div>
-  )
+      <div className ="list">
+        <h3> { 글제목[0] } <span onClick={함수()}>👍</span>  </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className ="list"> 
+        <h3> { 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className ="list"> 
+        <h3> { 글제목[2] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>  
+   </div>
+   
+  );
 }
 
 export default App;
