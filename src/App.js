9dbb19636e6d965309985f2779acab0459d90d07
corazon4815 <mango4815@knou.ac.kr>
2021-03-03 12:24:27 +0900

    modal만드는중

--- src/App.js
+++ src/App.js
@@ -2,36 +2,32 @@
 import react, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App(){
-  
-  let [글제목, 글제목변경] = useState(['남자 코트 추천','우동 맛집','여자 코트 추천']);
+  //여기 있는 state는 건들지말고 복사본으로 변경함수를 이용해서 변경하는게 좋음
+  let [글제목, 글제목변경] = useState(['남자코트 추천','우동 맛집','여자 코트 추천']);
   let [따봉, 따봉변경] = useState(0);
   let posts = '개발공부 글';
 
-  function 함수(){
-    console.log(1);
+  function 제목바꾸기(){
+    let newArray = [...글제목]; //딥카피
+    newArray = newArray.sort();
+    글제목변경(newArray);
   }
-  () => {
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-      <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-    );
-  };
 
-  
-
+  /** 제목바꾸기 - 함수이름에 ()를 붙이면 안눌러도 실행이 되니까 ()를 빼야함 */
   return (
    <div className="App">
       <div className="black-nav"> 
         <div style={{ color:'white',fontSize:'30px'}}>개발 Blog</div>       
       </div>
+      
+      <button onClick={제목바꾸기}>버튼</button> 
+      
       <div className ="list">
-        <h3> { 글제목[0] } <span onClick={함수()}>👍</span>  </h3>
+        <h3> { 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1)}}>👍</span>{ 따봉 }</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
@@ -45,9 +41,22 @@ function App(){
         <p>2월 17일 발행</p>
         <hr/>
       </div>  
+      
+      <Modal/>
+
    </div>
    
   );
+}
+
+function Modal(){ //컴포넌트 만들기(대문자로시작) //html관리가 편해짐 
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
